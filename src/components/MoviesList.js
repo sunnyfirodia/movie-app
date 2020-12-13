3584390c86133986adf5c6d2081400ff4c45b189
sunnyfirodia <sunnyfirodiya5@gmail.com>
2020-12-13 10:55:47 +0530

    listing changes

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import MovieItem from './MovieItem';
+import { Link } from 'react-router-dom'
 
 const MoviesList = ({ movies, isLoading }) => {
+  if(movies.length < 1 && isLoading == false){
+    return(
+      <>
+      <h2 className="section-title"> 
+        No Movies Found ...:(
+      </h2>
+      </>
+    )
+  }
   return (
     <div className="movies-list">
       {isLoading ? (
@@ -15,6 +25,7 @@ const MoviesList = ({ movies, isLoading }) => {
       )}
     </div>
   );
+  
 };
 
 export default MoviesList;
